63f5996615610ed088dc1f417f5e7b7bd09227f1
blankuet <blancafeijoocruz@gmail.com>
2024-06-13 16:53:33 +0200

    Model 'event' in progress

---
+++ models/Event.model.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const eventSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: false,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      unique: false,
+      trim: true,
+      lowercase: true,
+    },
+    img:    {
+        type: Buffer,
+        contentType: String
+    },
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
