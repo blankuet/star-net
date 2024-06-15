353e1f6265a20196893878be67fad0cfab3650be
Javier Godoy <javiergodoymiguez@gmail.com>
2024-06-15 14:56:08 +0200

    tu puta madre

---
+++ routes/users.routes.js
@@ -0,0 +1,16 @@
+const express = require(`express`);
+const router = express.Router();
+const User = require(`../models/User.model`);
+const isLoggedIn = require("../middleware/isLoggedIn");
+const consoleLog = require("../middleware/consoleLog");
+
+router.get(`/`, isLoggedIn, consoleLog, (req, res) => {
+    User.find()
+    .then((data) => {
+        console.log(data);
+        res.render("users/users", { users: data });
+    })
+    .catch((err) => console.log(err));
+});
+
+module.exports = router;
