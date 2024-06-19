33443474d3298781bce0f2bbc35fcd6642eac7a4
Vituin <124085647+Vituin@users.noreply.github.com>
2024-06-19 21:47:23 +0200

    v1.3 profile delete succesfull no solved modify

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,50 +4,43 @@ const User = require(`../models/User.model`);
 const isLoggedIn = require("../middleware/isLoggedIn");
 const consoleLog = require("../middleware/consoleLog");
 
-router.get(`/`, isLoggedIn, consoleLog, (req, res) => {
-    User.find()
-        .then((data) => {
-            console.log(data);
-            res.render("users/users", { users: data, isAuthenticated: !!req.session.currentUser });
-        })
-        .catch((err) => console.log(err));
-});
+router.get('/profile', isLoggedIn, consoleLog, (req, res) => {
+    const currentUser = req.session.currentUser;
 
-router.get(`/profile`, isLoggedIn, consoleLog, (req, res) => {
-    User.find()
+    User.findById(currentUser._id)
         .then((data) => {
             console.log(data);
-            res.render("users/profile", { users: data, isAuthenticated: !!req.session.currentUser });
+            res.render("users/profile", { user: data, isAuthenticated: !!req.session.currentUser });
         })
         .catch((err) => console.log(err));
 });
 
 router.get(`/:id`, isLoggedIn, (req, res) => {
-    User.findById(req.params.id).populate(`user`)
+    User.findById(req.params.id)
         .then((data) => {
             res.render(`users/profile`, { user: data });
-        });
+        })
+        .catch((err) => console.log(err));
 });
 
 router.post(`/:id/delete`, isLoggedIn, (req, res) => {
-    User.findByIdAndDelete(req.params.id).then(() => res.redirect(`auth/signup`));
+    User.findByIdAndDelete(req.params.id).then(() => res.redirect(`auth/signup`))
+        .catch(err => console.log(err));
 });
 
-router.get(`/edit/:id`, isLoggedIn, (req, res) => {
-    Promise.all([
-        User.findById(req.params.id),
-        User.find()
-    ])
-
-        .then(([user]) => {
-            res.render(`users/edit`, { user });
-        });
+router.get('/:id/edit', isLoggedIn, (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            res.render('users/edit-profile', { user });
+        })
+        .catch(err => console.log(err));
 });
 
 router.post(`/:id/edit`, isLoggedIn, (req, res) => {
     const { alias, email, username } = req.body;
     User.findByIdAndUpdate(req.params.id, { alias, email, username }, { new: true })
-        .then(() => res.redirect(`/users/${req.params.id}`));
+        .then(() => res.redirect(`/users/`))
+        .catch(err => console.log(err));
 });
 
 module.exports = router;
