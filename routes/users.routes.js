9b18017ec6862aaf2e75329715c8384a16acc572
Vituin <124085647+Vituin@users.noreply.github.com>
2024-06-20 14:48:54 +0200

    v1.4 bostrap card and responsive. no solved modify and solved delete

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,6 +4,15 @@ const User = require(`../models/User.model`);
 const isLoggedIn = require("../middleware/isLoggedIn");
 const consoleLog = require("../middleware/consoleLog");
 
+router.get(`/`, isLoggedIn, consoleLog, (req, res) => {
+    User.find()
+        .then((data) => {
+            console.log(data);
+            res.render("users/users", { users: data, isAuthenticated: !!req.session.currentUser });
+        })
+        .catch((err) => console.log(err));
+});
+
 router.get('/profile', isLoggedIn, consoleLog, (req, res) => {
     const currentUser = req.session.currentUser;
 
@@ -15,31 +24,19 @@ router.get('/profile', isLoggedIn, consoleLog, (req, res) => {
         .catch((err) => console.log(err));
 });
 
-router.get(`/:id`, isLoggedIn, (req, res) => {
-    User.findById(req.params.id)
-        .then((data) => {
-            res.render(`users/profile`, { user: data });
-        })
-        .catch((err) => console.log(err));
-});
+
 
 router.post(`/:id/delete`, isLoggedIn, (req, res) => {
     User.findByIdAndDelete(req.params.id).then(() => res.redirect(`auth/signup`))
         .catch(err => console.log(err));
 });
 
-router.get('/:id/edit', isLoggedIn, (req, res) => {
-    User.findById(req.params.id)
-        .then(user => {
-            res.render('users/edit-profile', { user });
-        })
-        .catch(err => console.log(err));
-});
+
 
 router.post(`/:id/edit`, isLoggedIn, (req, res) => {
     const { alias, email, username } = req.body;
     User.findByIdAndUpdate(req.params.id, { alias, email, username }, { new: true })
-        .then(() => res.redirect(`/users/`))
+        .then(() => res.redirect(`/users/edit-profile`))
         .catch(err => console.log(err));
 });
 
